1e150365e2020ff69986e60387002ba4b57b9f07
Marius Mirzac <mirzacmarius@gmail.com>
2016-11-12 01:40:02 +0200

    Added tasks list

---
+++ tasks.js
@@ -0,0 +1,53 @@
+Header {
+	Header actions {
+		Style lwa form
+		Add actions to form
+	}
+	Header logi
+	Header menu
+	Menu button/button animation
+	Menu cart items {
+		Close cart menu button
+		Show menu items with woo ajax
+
+	}
+	Menu show/hide
+	Menu current menu item
+	Hero image {
+		Featured image
+		Slider
+		Revolution slider
+		Slick slider
+		Layer slider
+	}
+
+}
+Sections {
+	Our farm
+	Video background
+	Visit us
+	What we do / woocommerce shortcode styling
+	Our products {
+		Portfolio slider
+	}
+	Our story
+	Blog posts slider
+	Team members
+	Meet Team
+	Newsletter
+}
+Pages {
+	Home
+	About us
+	Features
+	Portfolio
+	Blog
+	Shop
+	Contact us
+}
+Footer {
+	Logo
+	Menu
+	Socials
+	Copyright
+}
